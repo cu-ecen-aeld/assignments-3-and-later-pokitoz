135d50806d4f3632890c80c6f0b4aa7ff547f438
Florian Depraz <florian.depraz@outlook.com>
2025-02-25 14:46:07 +0000

    Add writer script
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Error: Missing arguments"
+    exit 1
+fi
+
+writefile="$1"
+writestr="$2"
+
+mkdir -p $(dirname "$writefile")
+touch "$writefile"
+echo "$writestr" > $writefile
+if [ $? -ne 0 ]; then
+    echo "Error: Could not create file"
+    exit 1
+fi
+exit 0
