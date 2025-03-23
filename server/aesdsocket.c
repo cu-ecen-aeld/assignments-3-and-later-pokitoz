b1da067ae09cb93f4696d6d59f74c8ad9c3ad837
Florian Depraz <florian.depraz@outlook.com>
2025-03-23 21:20:18 +0000

    Add timer and threading
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,25 +12,49 @@
 #include <signal.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/queue.h>
 
 static volatile bool end = false;
 int retval = -1;
-const char C_FILEPATH [] = "/var/tmp/aesdsocketdata";
+const char C_FILEPATH[] = "/var/tmp/aesdsocketdata";
 const char C_PORT[] = "9000";
 const int C_CONNECTION_REQUEST_QUEUE_SIZE = 10;
+pthread_mutex_t lock;
+FILE *fp = NULL;
+timer_t timerid;
+
+typedef struct thread_info
+{
+    pthread_t thread_id;
+    int thread_num;
+    char strIp[INET6_ADDRSTRLEN];
+    int socketId;
+    FILE *fd;
+    pthread_mutex_t *lock;
+} thread_info;
+
+struct slist_data
+{
+    thread_info *data;
+    SLIST_ENTRY(slist_data)
+    entries;
+};
 
 /**
  * Return the IP Address of the client depending if it is ipv4 or ipv6
  * @param sa
  * @return pointer to the ip address array. Valid until sa is valid
  */
-void* get_in_addr(struct sockaddr_storage* sa)
+void *get_in_addr(struct sockaddr_storage *sa)
 {
-    if (((struct sockaddr*) sa)->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*) sa)->sin_addr);
+    if (((struct sockaddr *)sa)->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
     }
 
-    return &(((struct sockaddr_in6*) sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
 /**
@@ -39,19 +63,171 @@ void* get_in_addr(struct sockaddr_storage* sa)
  */
 void signal_handler(int signal)
 {
+    printf("Signal\n");
+
     syslog(LOG_INFO, "Caught signal, exiting");
     end = true;
     retval = 0;
 }
 
-
-int main(int argc, char** argv)
+/**
+ * Callback function to handle a specific signal
+ * @param signal signal caught
+ */
+void signal_timer_handler(int signal)
 {
-    FILE* fp = NULL;
+    // char prefix[] = "timestamp:";
+    if (fp != NULL)
+    {
+        pthread_mutex_lock(&lock);
+        // fwrite(prefix, sizeof(char), strlen(prefix), fp);
+        // fwrite(asctime(timeinfo), sizeof(char), strlen(asctime(timeinfo)), fp);
+        // fflush(fp);
+        pthread_mutex_unlock(&lock);
+    }
+
+    printf("Timer expired\n");
+
+    syslog(LOG_INFO, "timer");
+}
+
+static void *thread_handler(void *arg)
+{
+
+    thread_info *tinfo = (thread_info *)arg;
+
+    syslog(LOG_INFO, "Accepted connection from %s", tinfo->strIp);
+    char buf[1024] = {0};
+
+    pthread_mutex_lock(tinfo->lock);
+
+    // Get the current position of the file
+    long current = 0;
+    // ftell(tinfo->fd);
+
+    while (!end)
+    {
+        int numbytes = recv(tinfo->socketId, buf, sizeof(buf) - 1, 0);
+
+        if (numbytes == -1)
+        {
+            syslog(LOG_ERR, "recvfrom error %d", numbytes);
+            continue;
+        }
+        buf[numbytes] = '\0';
+
+        if (fwrite(buf, sizeof(char), numbytes, tinfo->fd) != numbytes)
+        {
+            syslog(LOG_ERR, "Failed to write to file");
+            break;
+        }
+
+        printf("Got a total of %d\n", numbytes);
+        if (buf[numbytes - 1] == '\n')
+        {
+            fflush(tinfo->fd);
+            memset(buf, 0, sizeof(buf));
+            fseek(tinfo->fd, current, SEEK_SET);
+            break;
+        }
+    }
+
+    // Read back the file and send it through the socket
+    int numbytes = 1;
+    while (numbytes > 0)
+    {
+        numbytes = fread(buf, sizeof(char), sizeof(buf), tinfo->fd);
+        if (numbytes == 0)
+        {
+            // There is nothing to send
+            break;
+        }
+
+        // Send the data back
+        if (send(tinfo->socketId, buf, numbytes, 0) == -1)
+        {
+            syslog(LOG_ERR, "send error");
+            break;
+        }
+    }
+
+    syslog(LOG_INFO, "Closed connection from %s", tinfo->strIp);
+
+    pthread_mutex_unlock(tinfo->lock);
+
+    return NULL;
+}
+
+void maskTimer(int signal, bool stop)
+{
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, signal);
+
+    if (stop)
+    {
+        if (sigprocmask(SIG_SETMASK, &mask, NULL) == -1)
+        {
+            return;
+        }
+    }
+    else
+    {
+        if (sigprocmask(SIG_UNBLOCK, &mask, NULL) == -1)
+        {
+            return;
+        }
+    }
+}
+
+void timer_start(void)
+{
+    struct sigevent sev;
+    struct sigaction sa;
+
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = SIGRTMIN;
+    sev.sigev_value.sival_ptr = &timerid;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_handler = signal_timer_handler;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(sev.sigev_signo, &sa, NULL) == -1)
+    {
+        return;
+    }
+
+    maskTimer(sev.sigev_signo, true);
+
+    if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1)
+    {
+        return;
+    }
+
+    struct itimerspec its = {0};
+    its.it_value.tv_sec = 10;
+    its.it_value.tv_nsec = 0;
+    its.it_interval.tv_sec = its.it_value.tv_sec;
+    its.it_interval.tv_nsec = 0;
+
+    if (timer_settime(timerid, 0, &its, NULL) == -1)
+    {
+        return;
+    }
+
+    maskTimer(sev.sigev_signo, false);
+}
+
+int main(int argc, char **argv)
+{
     int new_fd = -1;
     int listenFd = -1;
 
-    openlog ("aesdsocket", 0, LOG_USER);
+    openlog("aesdsocket", 0, LOG_USER);
+
+    SLIST_HEAD(slisthead, slist_data)
+    head = SLIST_HEAD_INITIALIZER(head);
+    SLIST_INIT(&head);
 
     for (;;)
     {
@@ -64,22 +240,23 @@ int main(int argc, char** argv)
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_flags = AI_PASSIVE;
-        struct addrinfo* servinfo;
+        struct addrinfo *servinfo;
 
         retval = getaddrinfo(NULL, C_PORT, &hints, &servinfo);
-        if (retval != 0) {
+        if (retval != 0)
+        {
             syslog(LOG_ERR, "getaddrinfo: %s", gai_strerror(retval));
             retval = -1;
             break;
         }
 
-        for (struct addrinfo* p = servinfo; p != NULL; p = p->ai_next)
+        for (struct addrinfo *p = servinfo; p != NULL; p = p->ai_next)
         {
             syslog(LOG_INFO, "Attempt to bind");
             listenFd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
             if (listenFd == -1)
             {
-                syslog (LOG_ERR, "server: socket: %s", gai_strerror(retval));
+                syslog(LOG_ERR, "server: socket: %s", gai_strerror(retval));
                 // Try to find other address
                 continue;
             }
@@ -89,7 +266,7 @@ int main(int argc, char** argv)
             retval = setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &reuseAddr, sizeof(reuseAddr));
             if (retval == -1)
             {
-                syslog (LOG_ERR, "sock flag issue");
+                syslog(LOG_ERR, "sock flag issue");
                 break;
             }
 
@@ -97,7 +274,7 @@ int main(int argc, char** argv)
             if (retval == -1)
             {
                 close(listenFd);
-                syslog (LOG_ERR, "server: bind: %s", gai_strerror(retval));
+                syslog(LOG_ERR, "server: bind: %s", gai_strerror(retval));
                 break;
             }
 
@@ -117,12 +294,12 @@ int main(int argc, char** argv)
             int pid = fork();
             if (pid == -1)
             {
-                syslog (LOG_ERR, "Fork issue");
+                syslog(LOG_ERR, "Fork issue");
                 exit(-1);
             }
             else if (pid == 0)
             {
-                syslog (LOG_INFO, "Child Process launched");
+                syslog(LOG_INFO, "Child Process launched");
             }
             else
             {
@@ -131,7 +308,6 @@ int main(int argc, char** argv)
             }
         }
 
-        // We binded successfully and now we need to fork
         if (listen(listenFd, C_CONNECTION_REQUEST_QUEUE_SIZE) == -1)
         {
             perror("listen");
@@ -156,6 +332,12 @@ int main(int argc, char** argv)
             break;
         }
 
+        if (pthread_mutex_init(&lock, NULL) != 0)
+        {
+            syslog(LOG_ERR, "mutex init issue");
+            return 1;
+        }
+
         fp = fopen(C_FILEPATH, "a+");
         if (fp == NULL)
         {
@@ -163,76 +345,79 @@ int main(int argc, char** argv)
             break;
         }
 
-        struct sockaddr_storage their_addr;
-        socklen_t sinSize = sizeof(their_addr);
-        char strIp[INET6_ADDRSTRLEN] = {0};
+        timer_start();
 
-        while(!end)
+        int id = 0;
+        while (!end)
         {
+            socklen_t sinSize = sizeof(struct sockaddr_storage);
+            struct sockaddr_storage their_addr = {0};
+
+            // For some reasons, the accept call hangs when the timer expires
+            // So we need to check if the timer expired and break the loop
+
             new_fd = accept(listenFd, (struct sockaddr *)&their_addr, &sinSize);
             if (new_fd == -1)
             {
-                syslog(LOG_ERR, "Accept issue");
+                if (errno == EINTR && !end)
+                {
+                    continue;
+                }
+                syslog(LOG_ERR, "Accept issue %d", errno);
                 break;
             }
 
-            inet_ntop(their_addr.ss_family, get_in_addr(&their_addr),
-                      strIp, sizeof(strIp));
-
-            syslog(LOG_INFO, "Accepted connection from %s", strIp);
-            char buf[1024] = {0};
-
-            while(!end)
+            thread_info *tinfo = (thread_info *)malloc(sizeof(thread_info));
+            if (tinfo == NULL)
             {
-                int numbytes = recv(new_fd, buf, sizeof(buf) - 1, 0);
-
-                if (numbytes == -1)
-                {
-                    syslog(LOG_ERR, "recvfrom error %d", numbytes);
-                    continue;
-                }
-                buf[numbytes] = '\0';
-
-                if (fwrite(buf, sizeof(char), numbytes, fp) != numbytes)
-                {
-                    syslog(LOG_ERR, "Failed to write to file");
-                    break;
-                }
-
-                printf("Got %d ", numbytes);
-                if (buf[numbytes - 1] == '\n')
-                {
-                    fflush(fp);
-                    memset(buf, 0, sizeof(buf));
-                    fseek(fp, 0, SEEK_SET);
-                    break;
-                }
+                syslog(LOG_ERR, "Failed to allocate memory");
+                break;
             }
 
-            // Read back the file and send it through the socket
+            inet_ntop(their_addr.ss_family,
+                      get_in_addr(&their_addr), tinfo->strIp, sizeof(tinfo->strIp));
+            tinfo->thread_num = id++;
+            tinfo->socketId = new_fd;
+            tinfo->fd = fp;
+            tinfo->lock = &lock;
+            // And now we spawn a thread for handling this connection
 
-            int numbytes = 1;
-            while (numbytes > 0)
+            retval = pthread_create(&tinfo->thread_id, NULL, &thread_handler, tinfo);
+            if (retval != 0)
             {
-                numbytes = fread(buf, sizeof(char), sizeof(buf), fp);
-                if (numbytes == 0)
-                {
-                    // There is nothing to send
-                    break;
-                }
-
-                // Send the data back
-                if (send(new_fd, buf, numbytes, 0) == -1)
-                {
-                    syslog(LOG_ERR, "send error");
-                    break;
-                }
+                syslog(LOG_ERR, "Thread create issue");
+                break;
             }
 
-            syslog(LOG_INFO, "Closed connection from %s", strIp);
+            struct slist_data *item = (struct slist_data *)malloc(sizeof(struct slist_data));
+            if (item == NULL)
+            {
+                syslog(LOG_ERR, "Failed to allocate memory");
+                break;
+            }
+
+            item->data = tinfo;
+            SLIST_INSERT_HEAD(&head, item, entries);
         }
 
         break;
+    }
+
+    // Free the item in the queue
+    struct slist_data *item;
+    while (!SLIST_EMPTY(&head))
+    {
+        item = SLIST_FIRST(&head);
+        SLIST_REMOVE_HEAD(&head, entries);
+        pthread_join(item->data->thread_id, NULL);
+        free(item->data);
+        free(item);
+    }
+
+    struct itimerspec its = {0};
+    if (timer_settime(timerid, 0, &its, NULL) == -1)
+    {
+        syslog(LOG_ERR, "Failed to set timer");
     }
 
     if (fp != NULL)
@@ -247,9 +432,18 @@ int main(int argc, char** argv)
             syslog(LOG_ERR, "Failed to remove file");
         }
     }
-    close(listenFd);
-    close(new_fd);
+    if (listenFd != -1)
+    {
+        close(listenFd);
+    }
 
+    if (new_fd != -1)
+    {
+        close(new_fd);
+    }
+
+    pthread_mutex_destroy(&lock);
+    timer_delete(timerid);
     closelog();
     return retval;
 }
